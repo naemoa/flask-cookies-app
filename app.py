6241374aefc46b7074c0e57fe9af6f9653a5069a
naemoa <frank.mujuriz@gmail.com>
2024-06-20 23:43:47 +0300

    try

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 @app.route('/popup')
 def popup():
